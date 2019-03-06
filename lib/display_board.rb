674aa90f77785fc15101c3e6c4ef2af47815b603
leanne strickler <leanne.strickler@gmail.com>
2019-03-06 17:19:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,7 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts " #{board[4]} | #{board[4]} | #{board[4]}  "
-  puts "-----------"
-  puts " #{board[4]} | #{board[4]} | #{board[4]}  "
-  puts "-----------"
-  puts " #{board[4]} | #{board[4]} | #{board[4]}  "
-end
+     puts " #{board[0]} | #{board[1]} | #{board[2]} "
+     puts "-----------"
+     puts " #{board[3]} | #{board[4]} | #{board[5]} "
+     puts "-----------"
+     puts " #{board[6]} | #{board[7]} | #{board[8]} "
+   end
